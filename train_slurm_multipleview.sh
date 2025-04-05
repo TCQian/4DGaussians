5f2cd6b202d266c0b3bffdd0abbe36e595da35e6
Tan Chia Qian <66521590+TCQian@users.noreply.github.com>
2025-04-05 17:14:10 +0800

    update script

--- train_slurm_multipleview.sh
+++ train_slurm_multipleview.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=BouncingBals          # Job name
-#SBATCH --time=48:00:00                  # Time limit hrs:min:sec
+#SBATCH --job-name=MultipleView          # Job name
+#SBATCH --time=24:00:00                  # Time limit hrs:min:sec
 #SBATCH --gres=gpu:a100-40:1
 #SBATCH --mail-type=ALL                  # Get email for all status updates
 #SBATCH --mail-user=e0407638@u.nus.edu   # Email for notifications
@@ -10,7 +10,7 @@
 source ~/.bashrc
 conda activate 4dg
 
-python train.py -s /home/e/e0407638/github/4DGaussians/data/multipleview/bearRun --port 6017 --expname "multipleview/bearRun" --configs /home/e/e0407638/github/4DGaussians/arguments/multipleview/default.py
+python train.py -s /home/e/e0407638/github/4DGaussians/dataset/multipleview/bearRun --port 6017 --expname "multipleview/bearRun" --configs /home/e/e0407638/github/4DGaussians/arguments/multipleview/default.py
 
 python render.py --model_path "output/multipleview/bearRun/"  --skip_train --configs arguments/multipleview/default.py
 
